261fd6486ca046aae6733aca1cd4775bc3ad68c3
brferreiram <ferreirasantos1976@uol.com.br>
2021-11-06 23:14:34 -0300

    update

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,8 +3,11 @@
 @section('title', 'Criar Novo Post')
 
 @section('content')
-    <h1>Cadastrar novo Post</h1>
-    <form action="{{route('posts.store')}}" method="post" enctype="multipart/form-data">
-        @include('admin.posts._partials.form')
-    </form>
+    <h1 class="text-center text-3x1 uppercase font-black my-4">Novo Post</h1>
+
+    <div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12 mx-auto">
+        <form action="{{route('posts.store')}}" method="post" enctype="multipart/form-data">
+            @include('admin.posts._partials.form')
+        </form>
+    </div>
 @endsection
